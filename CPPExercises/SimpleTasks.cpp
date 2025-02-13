7570683887137895164b6c2600f3e479c3f3f6f5
Augustine Aguworumba <augustine.aguworumba@gmail.com>
2025-02-13 10:36:23 -0800

    Tutorial Exercises

---
+++ CPPExercises/SimpleTasks.cpp
@@ -0,0 +1,68 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+
+/*//          Variables and Data Types
+
+//Use variables to store different data of a college student:
+string name = "August";
+float regNo = 686196;
+string course = "Robotics";
+bool ageConfirmation = 1;
+int age = 29;
+char studentGrade = 'B';
+cout << "My details are: " << "\n"; 
+cout << "Name: " << name << "\n"; 
+cout << "Course: " << course << "\n"; 
+cout << "Age: " << age << "\n"; 
+cout << "Grade: " << studentGrade << "\n\n"; */
+
+
+/*//Calculate the area of a rectangle (by multiplying the length and width):
+float length;
+float width; 
+float area;
+cout << "Choose a number: ";
+cin >> length;
+cout << "Choose another number: ";
+cin >> width;
+area = length * width;
+cout << "The area of the rectangle is " << area << "\n\n";*/
+
+
+/*//Use the weekday number to calculate and output the weekday name:
+int weekdayNumber;
+cout << "Choose a number between 0 and 6: ";
+cin >> weekdayNumber;
+
+switch (weekdayNumber) {
+    case 0:
+    cout << "Today is Sunday \n";
+        break;
+    case 1:
+    cout << "Today is Monday \n";
+        break;
+    case 2:
+    cout << "Today is Tuesday \n";
+        break;
+    case 3:
+    cout << "Today is Wednesday \n";
+        break;
+    case 4:
+    cout << "Today is Thursday \n";
+        break;
+    case 5:
+    cout << "Today is Friday \n";
+        break;
+    case 6:
+    cout << "Today is Saturday \n";
+        break;
+    default:
+        cout << "Oops...Wrong Selection \n";
+}*/
+
+
+return 0;
+}
+
