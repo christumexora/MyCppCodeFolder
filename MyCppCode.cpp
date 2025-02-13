c6d75e0ddc0257f0c1acd013dd367225c1e2ae17
Augustine Aguworumba <augustine.aguworumba@gmail.com>
2025-02-13 09:11:38 -0800

    Alogoritm Tutorial

--- MyCppCode.cpp
+++ MyCppCode.cpp
@@ -1022,11 +1022,25 @@ vector<int> numbs = {1, 7, 3, 5, 9, 2};
   
   cout << *findGrtAlgo << "\n";*/
   
-
-  // FIND THE SMALLEST NUMBER
+/*// FIND THE SMALLEST NUMBER
   auto findMinAlgo = min_element(numbers.begin(), numbers.end());
   
-  cout << *findMinAlgo << "\n";
+  cout << *findMinAlgo << "\n";*/
+  
+//    MODIFYING ALGORITHM 
+//To copy elements from one vector to another, you can use the copy() function:
+
+// Create a vector called copiedNumbers that should store 6 integers
+vector<int> copiedNumbers(6);
+  
+// Copy elements from numbers to copiedNumbers
+copy(numbs.begin(), numbs.end(), copiedNumbers.begin()); 
+
+// Print elements of copiedNumbers 
+  for (int num : copiedNumbers) {
+    cout << num << "\n";
+  }
+  
   
 return 0;
 }
