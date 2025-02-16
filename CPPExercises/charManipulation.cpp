bfe6bfd8aa53605a3fff5bd5deef34b02f52ab42
Augustine Aguworumba <augustine.aguworumba@gmail.com>
2025-02-16 06:19:00 -0800

    Adding first three char to the front and back

--- CPPExercises/charManipulation.cpp
+++ CPPExercises/charManipulation.cpp
@@ -1,3 +1,4 @@
+using namespace std;
 #include <iostream>
 #include <string>
 #include <vector>
@@ -8,8 +9,9 @@
 #include <set>
 #include <map>
 #include <algorithm>
-using namespace std;
 
+
+/*
 int main(){
     string str;
     cout << "Please choose any word: ";
@@ -20,6 +22,23 @@ int main(){
     charVec.insert(charVec.begin(), lastChar);
     charVec.push_back(lastChar);
 
+    for(char c : charVec){
+        cout << c;
+    }
+   cout << endl;
+    return 0;
+}*/
+
+
+int main(){
+      string str;
+    cout << "Please choose any word: ";
+    cin >> str;
+
+    string firstThreeChar = str.substr(0, 3);;
+    vector<char> charVec(str.begin(), str.end());
+    charVec.insert(charVec.begin(), firstThreeChar.begin(), firstThreeChar.end());
+    charVec.insert(charVec.end(), firstThreeChar.begin(), firstThreeChar.end());
     for(char c : charVec){
         cout << c;
     }
